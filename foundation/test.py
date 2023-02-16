4d97df0aa35c899134d22bdaab21bf16a650adfc
do-something-learn <3237235868@qq.com>
2023-02-16 09:07:15 +0800

    this is a  project

--- foundation/test.py
+++ foundation/test.py
@@ -1,23 +1,58 @@
 import time
 import requests
 import logging
-
-# times=int(time.time())#获取时间戳
-# import hashlib
-# value=hashlib.sha1("/api/movie,{times}".encode('utf-8')).hexdigest()
-# print(value)
-# import base64
-# bsvalue=base64.b64encode((value+',{times}').encode('utf-8')).decode('utf-8')
-# print(bsvalue)
-# url=f'https://spa6.scrape.center/api/movie/?limit=10&offset=0&token={bsvalue}'
-# print(url)
-# response=requests.get(url=url)
-# print(response.json(),response.text)
-
-# times=int(time.time())#获取时间戳
+from fake_useragent import UserAgent
 import hashlib
-value=hashlib.sha1("/api/movie,{times}".encode('utf-8')).hexdigest()
-print(value)
+import time
 import base64
-bsvalue=base64.b64encode((value+',{times}').encode('utf-8')).decode('utf-8')
-print(bsvalue)
+import copy
+#获取token
+def get_token(args:list):
+    
+    timestamp=str(int(time.time()))
+    newargs=copy.deepcopy(args)
+    newargs.append(timestamp)
+    value=hashlib.sha1(','.join(newargs).encode('utf-8')).hexdigest()
+    # print(value)
+    token=base64.b64encode((f'{value},{timestamp}').encode('utf-8')).decode('utf-8')
+    # print(token)
+    return token
+
+def scrapy_method(url): 
+    ua=UserAgent()
+    response=requests.get(url=url,headers={'user-agent':ua.random})
+    return response
+ 
+
+LIMIT=10
+OFFSET=0
+id_list=[]
+URL='https://spa6.scrape.center/api/movie/?limit={limit}&offset={offset}&token={token}'
+args=['/api/movie']
+#遍历页面获取id
+for page in range(1,10):
+   offset=LIMIT*(page-1)
+   token=get_token(args)
+   print(token)
+   url=URL.format(limit=LIMIT,offset=offset,token=token)#获取列表页url
+   response=scrapy_method(url)
+ 
+   for item in response.json()['results']:
+        
+        id_list.append(item['id'])
+    
+   
+
+# 遍历集合，获取id
+for item in id_list:
+    #根据id进行拼接字符串
+    id_value='ef34#teuq0btua#(-57w1q5o5--j@98xygimlyfxs*-!i-0-mb'+str(item)
+    #进行base64编码
+    key=base64.b64encode(id_value.encode('utf-8')).decode('utf-8')
+    #将编码后的值进行拼接，作为参数
+    arg=[f'/api/movie/{key}']
+    token=get_token(arg)
+    URL_DETAIL='https://spa6.scrape.center/api/movie/{key}/?token={token}'
+    url_detail=URL_DETAIL.format(key=key,token=token)
+    response=scrapy_method(url_detail)
+    print(response.json()['name'])
